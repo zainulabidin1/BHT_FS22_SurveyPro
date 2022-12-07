434a7f392b66b9e96984d65522f73badde1f0431
Zain <zain.ul.abidin@outlook.com>
2022-12-07 17:46:38 +0500

    survey viewer added

---
+++ SurveyPro/Controllers/SurveyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyPro.MVC.Controllers
+{
+    public class SurveyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("SurveyManagement", "Survey");
+        }
+
+        public IActionResult SurveyManagement()
+        {
+            return View();
+        }
+
+        public IActionResult SurveyEditor()
+        {
+            return View();
+        }
+    }
+}
---
+++ SurveyPro/Controllers/SurveyViewerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SurveyPro.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyPro.Controllers
+{
+    public class SurveyViewerController : Controller
+    {
+        private readonly ILogger<SurveyViewerController> _logger;
+
+        public SurveyViewerController(ILogger<SurveyViewerController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
